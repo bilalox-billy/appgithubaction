7f384179176c0043d3d6328bb4403319d73b305d
Bilal Ben Mahria <bilal.benmahria.up@gmail.com>
2025-08-03 17:27:33 +0000

    Update test_math_operation.py

--- tests/test_math_operation.py
+++ tests/test_math_operation.py
@@ -1,4 +1,4 @@
-from src.math_operation import add, subtract
+from src.math_operation import add, sub
 
 def test_add():
     assert add(2, 3) == 5
@@ -6,6 +6,6 @@ def test_add():
     assert add(0, 0) == 0
 
 def test_subtract():
-    assert subtract(5, 3) == 2
-    assert subtract(0, 0) == 0
-    assert subtract(10, 5) == 5
+    assert sub(5, 3) == 2
+    assert sub(0, 0) == 0
+    assert sub(10, 5) == 5
